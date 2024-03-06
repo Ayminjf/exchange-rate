8d091484f7503f65ecfca19c773fce9fa3fd5011
Aymin <ayminjf@gmail.com>
2024-03-06 19:22:36 +0330

    feat(screens): add screens folder

---
+++ lib/screens/currency_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CurrencyScreen extends StatefulWidget {
+  const CurrencyScreen({super.key});
+
+  @override
+  State<CurrencyScreen> createState() => _CurrencyScreenState();
+}
+
+class _CurrencyScreenState extends State<CurrencyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

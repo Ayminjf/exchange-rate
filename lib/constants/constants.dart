b48a84a7a432f7fa227063a926cf665196ba7433
Aymin <ayminjf@gmail.com>
2024-03-09 15:53:51 +0330

    feat(constants): add iconNavigationBar widget

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:exchange_rate/screens/home_screen.dart';
 import 'package:exchange_rate/screens/coin_screen.dart';
 import 'package:exchange_rate/screens/gold_screen.dart';
 import 'package:exchange_rate/screens/crypto_screen.dart';
@@ -8,40 +9,43 @@ class Constants {
   static Color blackColor = const Color(0xFF282828);
   static Color whiteColor = const Color(0xFFF0F0F0);
 
-  List<Widget> screens = const [
+  static List<Widget> screens = const [
     CurrencyScreen(),
     CoinScreen(),
+    HomeScreen(),
     GoldScreen(),
     CryptoScreen(),
   ];
 
-  List<Widget> iconNavigationBar = [
-    SizedBox(
-      height: 40,
-      width: 40,
-      child: Image.asset("assets/images/currencys.png"),
-    ),
-    SizedBox(
-      height: 40,
-      width: 40,
-      child: Image.asset("assets/images/coins.png"),
-    ),
-    SizedBox(
-      height: 40,
-      width: 40,
-      child: Image.asset("assets/images/golds.png"),
-    ),
-    SizedBox(
-      height: 40,
-      width: 40,
-      child: Image.asset("assets/images/cryptos.png"),
-    ),
-  ];
-
-  List<String> appBarTitle = const [
+  static List<String> appBarTitle = const [
     "نرخ ارز کشور ها",
     "نرخ سکه",
+    "خانه",
     "نرخ طلا",
     "نرخ ارزدیجیتال",
   ];
+
+  static List<Widget> iconNavigationBar = const [
+    CustomTabBar(image: "currencys"),
+    CustomTabBar(image: "coins"),
+    CustomTabBar(image: "golds"),
+    CustomTabBar(image: "cryptos"),
+  ];
+}
+
+class CustomTabBar extends StatelessWidget {
+  final String image;
+  const CustomTabBar({
+    required this.image,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      width: 30,
+      child: Image.asset("assets/images/$image.png"),
+    );
+  }
 }

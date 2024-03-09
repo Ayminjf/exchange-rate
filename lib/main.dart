f7e0f1b9fdd52d74ab91938edd268ee191def392
Aymin <ayminjf@gmail.com>
2024-03-09 18:38:47 +0330

    feat(main): hide android navigation bar buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:exchange_rate/screens/root_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:exchange_rate/constants/constants.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.top],
+  );
 }
 
 class MainApp extends StatelessWidget {

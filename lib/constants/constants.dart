4e59d2b6fef8fb892b18ad85de002a5499bdc6eb
Aymin <ayminjf@gmail.com>
2024-03-13 15:54:58 +0330

    feat(constants): add persian number extension

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -93,6 +93,18 @@ class Constants {
   ];
 }
 
+extension PersianNumberExtension on String {
+  String get persianNumber {
+    const english = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const persian = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    String text = this;
+    for (var i = 0; i < english.length; i++) {
+      text = text.replaceAll(english[i], persian[i]);
+    }
+    return text;
+  }
+}
+
 class CustomTabBar extends StatelessWidget {
   final String image;
   const CustomTabBar({

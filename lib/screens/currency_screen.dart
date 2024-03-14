d52e197f07a432edf3345728493012f30657fb8f
Aymin <ayminjf@gmail.com>
2024-03-14 12:09:35 +0330

    feat(currency-screen): add currency item widget & countries info widget

--- lib/screens/currency_screen.dart
+++ lib/screens/currency_screen.dart
@@ -1,4 +1,6 @@
+import 'package:exchange_rate/constants/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CurrencyScreen extends StatefulWidget {
   const CurrencyScreen({super.key});
@@ -16,6 +18,57 @@ class _CurrencyScreenState extends State<CurrencyScreen> {
         "Currency",
         style: TextStyle(color: Colors.black, fontSize: 25),
       )),
+class CurrencyItems extends StatelessWidget {
+  const CurrencyItems({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (context, index) {
+        return _buildCurrencyItem(context, index);
+      },
+      itemCount: Constants.flagCurrency.length,
+    );
+  }
+
+  Widget _buildCurrencyInfo(BuildContext context, int index) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(5),
+          decoration: const BoxDecoration(
+            color: Color(0xFFDDDDDD),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          child: SizedBox(
+            height: 55,
+            width: 55,
+            child: SvgPicture.asset(
+              "assets/images/${Constants.flagCurrency[index]}.svg",
+            ),
+          ),
+        ),
+        const SizedBox(width: 10),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              Constants.nameCurrency[index],
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              Constants.flagCurrency[index].toUpperCase(),
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
     );
   }
 }

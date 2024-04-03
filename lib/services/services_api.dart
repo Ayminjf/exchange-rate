dc0c10fc975693affda426aa816c0c6af49b4820
Aymin <ayminjf@gmail.com>
2024-04-03 05:06:50 +0330

    feat(services-api): add fetchPriceListData future method

---
+++ lib/services/services_api.dart
@@ -0,0 +1,28 @@
+import 'package:exchange_rate/model/crypto_currency.dart';
+import 'package:http/http.dart' as http;
+import 'package:exchange_rate/model/price_list.dart';
+import 'dart:convert';
+
+class ServicesApi {
+  Future<Map<String, Current>> fetchPriceListData() async {
+    try {
+      final response = await http.get(
+        Uri.parse("https://call1.tgju.org/ajax.json"),
+        headers: <String, String>{
+          'Accept': '*/*',
+          'charset': 'utf-8',
+          'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, Current> priceMap =
+            PriceListModel.fromJson(json.decode(response.body)).current;
+        return priceMap;
+      } else {
+        throw Exception('Failed to load price list');
+      }
+    } catch (e) {
+      throw Exception("An error occurred: $e");
+    }
+  }

bd6f7768decbd6f821779d34e4c2c7b0a3af7105
Aymin <ayminjf@gmail.com>
2024-04-07 19:40:57 +0330

    feat(home-screen): add currency home layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,12 +10,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        "Home",
-        style: TextStyle(color: Colors.black, fontSize: 25),
-      )),
-    );
-  }
+    Size size = MediaQuery.of(context).size;
+    return Localizations.override(
+      context: context,
+      locale: const Locale('fa'),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                Container(
+                  height: size.height * 0.3,
+                  decoration: BoxDecoration(
+                    color: Constants.greyColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: SizedBox(
+                      height: size.height * 0.75,
+                      child: const CurrencyItems(count: 3),
+                    ),
+                  ),
+                ),
 }

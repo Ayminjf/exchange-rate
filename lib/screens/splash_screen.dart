d2aea75f564f6b1b79157025e9b9d8ebda51581a
Aymin <ayminjf@gmail.com>
2024-05-09 00:19:57 +0330

    feat(provider,splash-screen): check the internet connection

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:exchange_rate/constants/constants.dart';
 import 'package:exchange_rate/screens/root_screens.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +22,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
+
     Provider.of<DataPriceListAndCrypto>(context, listen: false).getData();
   }
 
@@ -27,30 +31,69 @@ class _SplashScreenState extends State<SplashScreen> {
     final dplc = Provider.of<DataPriceListAndCrypto>(context);
     return Scaffold(
       backgroundColor: Constants.blackColor,
-      body: dplc.loading
-          ? const Center(
+      body: dplc.activeConnection == true
+          ? dataLoadingAnimation(dplc)
+          : Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    height: 350,
-                    width: 350,
-                    child: RiveAnimation.asset("assets/images/loading.riv"),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "لطفا کمی صبر کنید ...",
+                  const Text(
+                    "دستگاه به اینترنت متصل نیست ...",
                     style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: "lalezar",
+                      fontSize: 18,
+                      fontFamily: "iransans",
                       color: Colors.white,
+                    ),
+                  ),
+                  TextButton.icon(
+                    onPressed: () => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SplashScreen(),
+                        ),
+                        (route) => false),
+                    icon: const Icon(
+                      Icons.wifi_find_sharp,
+                      color: Colors.green,
+                    ),
+                    label: const Text(
+                      "دوباره تلاش کنید",
+                      style: TextStyle(
+                          fontFamily: "lalezar",
+                          fontSize: 18,
+                          color: Colors.green),
                     ),
                   )
                 ],
               ),
-            )
-          : const RootScreen(),
+            ),
     );
+  }
+
+  Widget dataLoadingAnimation(DataPriceListAndCrypto dplc) {
+    return dplc.loading
+        ? const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 350,
+                  width: 350,
+                  child: RiveAnimation.asset("assets/images/loading.riv"),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "لطفا کمی صبر کنید ...",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: "lalezar",
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : const RootScreen();
   }
 }
